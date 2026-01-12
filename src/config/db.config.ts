b3b4bb0e1a52de22e05a0ea8bf50ada11d8f4ba0
Ingyin-Phyo275 <xiluhan.igp@gmail.com>
2026-01-12 13:58:42 +0630

    New Change in db config

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,10 +2,14 @@ import { DataSource } from "typeorm";
 import { Question } from "../questions/entities/question.entity";
 import { Result } from "../results/entities/result.entity";
 
+if (!process.env.DATABASE_URL) {
+  console.error("❌ DATABASE_URL is missing");
+}
+
 export const AppDataSource = new DataSource({
-  type: "postgres", // ✅ MUST be hardcoded
-  url: process.env.DATABASE_URL, // ✅ Railway provides this
-  synchronize: true, // ⚠️ OK for now, disable later
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  synchronize: true,
   logging: true,
   entities: [Question, Result],
 });
