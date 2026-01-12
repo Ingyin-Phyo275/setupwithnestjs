cd9bfe043705e39bac6479445a5878bd12586ec8
Ingyin-Phyo275 <xiluhan.igp@gmail.com>
2026-01-12 13:55:13 +0630

    change in db config

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,18 +1,11 @@
 import { DataSource } from "typeorm";
 import { Question } from "../questions/entities/question.entity";
 import { Result } from "../results/entities/result.entity";
-import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
-    type: (process.env.TYPE as any) ,
-    host: process.env.HOST ,
-    port: Number(process.env.DB_PORT) ,
-    username: process.env.DB_USER ,
-    password: process.env.DB_PASS ,
-    database: process.env.DB_NAME ,
-    synchronize: true,
-    logging: true,
-    entities: [Question, Result],
-    subscribers: [],
-    migrations: [],
-})
+  type: "postgres", // ✅ MUST be hardcoded
+  url: process.env.DATABASE_URL, // ✅ Railway provides this
+  synchronize: true, // ⚠️ OK for now, disable later
+  logging: true,
+  entities: [Question, Result],
+});
