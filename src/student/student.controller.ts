ce3860d770a9fc58df299053f4dc1be743bdb4c1
Ingyin-Phyo275 <xiluhan.igp@gmail.com>
2026-02-25 14:30:27 +0630

    Add student count api

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
@@ -13,14 +13,22 @@ export class StudentController {
   }
 
   @Get()
-  findAll() {
-    return this.studentService.findAll();
+  findAll(@Query ('gender') gender?: string) {
+    return this.studentService.findAll(gender);
   }
 
+
+    @Get('count')
+  countApi() {
+    return this.studentService.countApi();
+  }
+  
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.studentService.findOne(id);
   }
+
+
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 import { AppDataSource } from '../config/db.config';
+import { stat } from 'fs';
 
 @Injectable()
 export class StudentService {
@@ -18,20 +19,114 @@ export class StudentService {
     }
   }
 
-  async findAll() {
+  async maleStudents() {
     try {
-      const res = await AppDataSource.getRepository('student').findAndCount();
+      const res = await AppDataSource.getRepository('student').findAndCount({
+        where: {
+          gender: 'male'
+        }
+      })
       const [data, count] = res;
-      return {
-        statusCode: 200,
-        message: 'Students fetched successfully',
-        data: data,
-        count: count
+      if(count === 0) {
+        return {
+          statusCode: 200,
+          message: 'No male student found',
+          data: data,
+          count: count
+        }
+      }else {
+        return {
+          statusCode: 200,
+          message: 'Male students fetched successfully',
+          data: data,
+          count: count
+        }
       }
     } catch (error) {
       throw new Error(error.message);
     }
   }
+
+  async femaleStudents() {
+    try {
+      const res = await AppDataSource.getRepository('student').findAndCount({
+        where: {
+          gender: 'female'
+        }
+      })
+      const [data, count] = res;
+      if(count === 0) {
+        return {
+          statusCode: 200,
+          message: 'No female student found',
+          data: data,
+          count: count
+        }
+      }else {
+        return {
+          statusCode: 200,
+          message: 'Female students fetched successfully',
+          data: data,
+          count: count
+        }
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async findAll(gender?: string) {
+    try {
+      const res = await AppDataSource.getRepository('student').findAndCount({
+        where: {
+          gender: gender
+        }
+      });
+
+      const male = await this.maleStudents();
+      const female = await this.femaleStudents();
+
+      if(res[0].length === 0) {
+        return {
+          statusCode: 200,
+          message: 'No student found',
+          data: res
+        }
+      }
+      const [data, count] = res;
+      return {
+        statusCode: 200,
+        message: 'Students fetched successfully',
+        data: data,
+        count: count,
+        male: male?.count,
+        female: female?.count
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async countApi () {
+    try {
+      const total = await this.findAll();
+      const male = await this.maleStudents();
+      const female = await this.femaleStudents();
+      const data = {
+        total: total?.count,
+        male: male?.count,
+        female: female?.count
+      }
+      return {
+        statusCode: 200,
+        message: 'Students fetched successfully',
+        data
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
 
   async findOne(id: string) {
     try {
